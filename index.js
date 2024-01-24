f0456bab976be03b42b694fd93684112930de4c1
Pruthviraj Chaudhari <chaudharipruthviraj888@gmail.com>
2024-01-24 11:28:57 +0530

    Updated CORS domains

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(
 	cors({
-		origin:["http://localhost:5173", "https://akatsuki-shadcn-frontend.vercel.app", "https://akatsuki-shadcn-frontend.vercel.app/#", "https://akatsuki-connect.vercel.app/#"],
+		origin:["http://localhost:5173", "https://akatsuki-shadcn-frontend.vercel.app", "https://akatsuki-shadcn-frontend.vercel.app/#", "https://akatsuki-connect.vercel.app/#/profiles"],
 		credentials:true,
 	})
 )
